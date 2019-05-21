efbcc79541236a5b0ca7854c7c2432ffef996b1f
BangYuWen <bangyuwen@gmail.com>
2019-05-21 22:59:46 +0800

    crawl page 2

--- crawler/crawl_legislator_info.py
+++ crawler/crawl_legislator_info.py
@@ -3,17 +3,26 @@ from sys import exit
 
 import requests
 
-LEGISLATOR_INFO_URL = 'http://data.ly.gov.tw/odw/openDatasetJson.action?id=16&selectTerm=all&page=1'
 FILE_DIR = path.dirname(path.abspath(__file__))
-OUTPUT_PATH = f'{FILE_DIR}/../data/legislator_info.json'
+MAX_PAGE_NUM = 2
 
-if __name__ == "__main__":
+
+def crawl_page(page_num):
+    print(f'[INFO] Start crawling page {page_num}')
     try:
-        response = requests.get(LEGISLATOR_INFO_URL, timeout=10)
+        legislator_info_url = f'http://data.ly.gov.tw/odw/openDatasetJson.action?id=16&selectTerm=all&page={page_num}'
+        response = requests.get(legislator_info_url, timeout=10)
     except requests.exceptions.ConnectTimeout:
-        exit('[ERROR] Request Error')
+        exit('[ERROR] Request Timeout')
 
-    assert response.status_code == 200, 'Request Error'
+    assert response.status_code == 200, f'[ERROR] Request Error, response code: {response.status_code}'
+
+    OUTPUT_PATH = f'{FILE_DIR}/../data/legislator_info_page{page_num}.json'
     with open(OUTPUT_PATH, 'w+') as fp:
         fp.write(response.text)
-        print(f'"{LEGISLATOR_INFO_URL}" is written to "{OUTPUT_PATH}"')
+        print(f'"{legislator_info_url}" is written to "{OUTPUT_PATH}"')
+
+
+if __name__ == "__main__":
+    for page in range(1, MAX_PAGE_NUM + 1):
+        crawl_page(page)
