d4e5ac70b3b0ce980460efd3de0ff96fdbe1efd3
BangYuWen <bangyuwen@gmail.com>
2019-05-23 09:38:14 +0800

    crawl current legislator page

--- crawler/crawl_legislator_info.py
+++ crawler/crawl_legislator_info.py
@@ -1,16 +1,17 @@
-from os import path
+from os import path, environ
 from sys import exit
 
 import requests
 
 FILE_DIR = path.dirname(path.abspath(__file__))
-OUTPUT_DIR = f'{FILE_DIR}/../data'
+OUTPUT_DIR = environ.get('OUTPUT_DIR', f'{FILE_DIR}/../data')
+URL = 'http://data.ly.gov.tw/odw/openDatasetJson.action'
 
 
-def send_request(url):
-    print(f'[INFO] Sending request to {url}')
+def send_request(payload):
+    print(f'[INFO] Sending request to {URL}')
     try:
-        response = requests.get(url, timeout=10)
+        response = requests.get(URL, params=payload, timeout=10)
     except requests.exceptions.ConnectTimeout:
         exit('[ERROR] Request Timeout')
 
@@ -22,16 +23,18 @@ def store_info(file_name, response_body):
     OUTPUT_PATH = f'{OUTPUT_DIR}/{file_name}'
     with open(OUTPUT_PATH, 'w+') as fp:
         fp.write(response_body)
-        print(f'response_body is written to "{OUTPUT_PATH}"')
+        print(f'[INFO] Response_body is written to "{OUTPUT_PATH}"')
 
 
-def crawl_legislator_info_pages(page_count=1):
+def crawl_legislator_info_pages(id, payload_base={}, page_count=1):
     for page_num in range(1, page_count + 1):
-        url = f'http://data.ly.gov.tw/odw/openDatasetJson.action?id=16&selectTerm=all&page={page_num}'
-        file_name = f'legislator_info_page{page_num}.json'
-        response_body = send_request(url)
+        print(f'[INFO] Start crawling "{id}" page{page_num} ')
+        payload = {**payload_base, 'page': page_num}
+        file_name = f'{id}_page{page_num}.json'
+        response_body = send_request(payload)
         store_info(file_name, response_body)
 
 
 if __name__ == "__main__":
-    crawl_legislator_info_pages(page_count=2)
+    crawl_legislator_info_pages('history_legislator_info', payload_base={'id': 16, 'selectTerm': 'all'}, page_count=2)
+    crawl_legislator_info_pages('current_legislator_info', payload_base={'id': 9, 'selectTerm': 'all'}, page_count=1)
