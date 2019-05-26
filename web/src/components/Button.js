5e9cc57f251bb28432035f68a20fd634e720c855
RilakuBear <bluesdollcat@gmail.com>
2019-05-26 18:43:18 +0800

    add first web

---
+++ web/src/components/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Button.css'; // Tell Webpack that Button.js uses these styles
+
+class Button extends Component {
+  render() {
+    // You can use them as regular CSS styles
+    return <div className="Button" />;
+  }
+}
+
+export default Button; // Don’t forget to use export default!
---
+++ web/src/components/DangerButton.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Button from './Button'; // Import a component from another file
+
+class DangerButton extends Component {
+  render() {
+    return <Button color="red" />;
+  }
+}
+
+export default DangerButton;
