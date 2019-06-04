e8fb7bdc33e381817e455dba213eeb4fc229583f
Terence Liu(劉庭瑋) <Terence_Liu@HTC.COM>
2019-06-04 13:51:18 +0800

    [config] added python3 in scripts

--- preprocessing/preprocess_all.sh
+++ preprocessing/preprocess_all.sh
@@ -5,5 +5,5 @@ set -o errexit
 SOURCE_DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "${SOURCE_DIR}"
 
-pipenv run python numbering_legislator
-pipenv run python generate_legislator_info
+pipenv run python3 numbering_legislator
+pipenv run python3 generate_legislator_info
