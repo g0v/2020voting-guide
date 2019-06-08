a1b1341e245677aabcecd3d58a294074c38b6140
terenzio <terenzio@gmail.com>
2019-06-08 23:52:13 +0800

    [feature] added AboutPage info card version 1 (#52)

--- web/src/components/AboutPage.jsx
+++ web/src/components/AboutPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import AboutInfoCard from './AboutInfoCard';
 
 const CandidatePage = () => (
   <div>
@@ -10,6 +11,7 @@ const CandidatePage = () => (
         共同作者
       </Typography>
       <Box m={1} />
+      <AboutInfoCard />
       <Typography variant="h5">溫邦宇</Typography>
       <Typography variant="h5">金柏仲</Typography>
       <Typography variant="h5">林佳鴻</Typography>
--- web/src/stories/index.js
+++ web/src/stories/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import InfoCard from '../components/InfoCard';
 import InfoDetalis from '../components/InfoDetails';
+import AboutInfoCard from '../components/AboutInfoCard';
 
 storiesOf('Legislator', module)
     .add('Overview', () => <InfoCard />)
