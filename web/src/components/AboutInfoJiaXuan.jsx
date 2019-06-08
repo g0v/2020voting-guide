ac210219c1a783da8b335ff0ba7e9cff5c43d0ab
Terence Liu(劉庭瑋) <Terence_Liu@HTC.COM>
2019-06-09 02:44:18 +0800

    [feature] add all placeholders for AboutUs page

--- web/src/components/AboutInfoJiaXuan.jsx
+++ web/src/components/AboutInfoJiaXuan.jsx
@@ -47,42 +47,41 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const AboutInfoCard = () => {
+const AboutInfoJiaXuan = () => {
   const classes = useStyles();
   return (
     <Card className={classes.card}>
       <CardMedia
         className={classes.cover}
-        image="/images/BangYu.jpg"
+        image="/images/JiaXuan.jpg"
         title="Live from space album cover"
       />
       <div className={classes.details}>
         <CardContent className={classes.content}>
           <Typography className={classes.name} component="h1" variant="h2">
-            溫邦宇
+            JiaXuan
           </Typography>
           <Typography className={classes.subtitle} variant="h5" color="textSecondary">
-            神的力量，18歲
+            FE
           </Typography>
           <Typography className={classes.description} variant="body2">
-            帥 <br />
-            Python 學霸 <br />
-            數據分析大師 <br />
-            Data Pipeline 小神童 <br />
-            人工智慧之父   <br />
-            <strike>單身 </strike> <br />
-             帥 <br />
+            1) Info <br />
+            2) Info <br />
+            3) Info <br />
+            4) Info <br />
+            5) Info <br />
+            6) Info <br />
             <br />
           </Typography>
         </CardContent>
       </div>
       <CardActions>
         <Button href="#outlined-buttons" className={classes.button}>
-          聯絡電話
+          Github
         </Button>
       </CardActions>
     </Card>
   );
 };
 
-export default AboutInfoCard;
+export default AboutInfoJiaXuan;
--- web/src/stories/index.js
+++ web/src/stories/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import InfoCard from '../components/InfoCard';
 import InfoDetalis from '../components/InfoDetails';
-import AboutInfoCard from '../components/AboutInfoCard';
 
 storiesOf('Legislator', module)
     .add('Overview', () => <InfoCard />)
