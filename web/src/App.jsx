d422f1d1ea3b719783c0837da187287104528272
pochung <pochung_chin@htc.com>
2019-06-11 21:56:49 +0800

    tab

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,46 +1,54 @@
 import Container from '@material-ui/core/Container';
-import IconButton from '@material-ui/core/IconButton';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
-import MenuIcon from '@material-ui/icons/Menu';
 import React from 'react';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import './App.css';
 import CandidatePage from './components/CandidatePage';
 import AboutPage from './components/AboutPage';
 import MapPage from './components/MapPage';
 import CandidateListPage from './components/CandidateListPage';
+import { makeStyles } from '@material-ui/core/styles';
 
 
-const App = () => (
-  <div className="App">
-    <BrowserRouter>
-      <Tabs
-        indicatorColor="primary"
-        textColor="primary"
-        centered
-      >
-        <IconButton
-          color="inherit"
-          aria-label="Open drawer"
-          // onClick={handleDrawerOpen}
-          edge="start"
+const useStyles = makeStyles(theme => ({
+  tab: {
+    fontSize: '28px'
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
         >
-          <MenuIcon />
-        </IconButton>
-        <Tab label="首頁" component={Link} to="/" />
-        <Tab label="地圖" component={Link} to="/map" />
-        <Tab label="立委候選人" component={Link} to="/candidate" />
-        <Tab label="關於" component={Link} to="/about" />
-      </Tabs>
-      <Container maxWidth="sm">
-        <Route exact path="/" component={CandidateListPage} />
-        <Route path="/map" component={MapPage} />
-        <Route path="/candidate" component={CandidatePage} />
-        <Route path="/about" component={AboutPage} />
-      </Container>
-    </BrowserRouter>
-  </div>
-);
+          <Tab className={classes.tab} label="首頁" component={Link} to="/" />
+          <Tab className={classes.tab} label="地圖" component={Link} to="/map" />
+          <Tab className={classes.tab} label="立委候選人" component={Link} to="/candidate" />
+          <Tab className={classes.tab} label="關於" component={Link} to="/about" />
+        </Tabs>
+        <Container maxWidth="sm">
+          <Route exact path="/" component={CandidateListPage} />
+          <Route path="/map" component={MapPage} />
+          <Route path="/candidate" component={CandidatePage} />
+          <Route path="/about" component={AboutPage} />
+        </Container>
+      </BrowserRouter>
+    </div>
+  );
+}
 
 export default App;
--- web/src/components/MapPage.jsx
+++ web/src/components/MapPage.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import Sidebar from './Sidebar';
 
 const CandidatePage = () => (
   <div>
+    <Sidebar />
     <img src="/images/TW-EPS-02-0003.jpg" alt="Taiwan" width="600"/>
     <a href="http://www.freevectormaps.com/taiwan/TW-EPS-02-0003?ref=atr">Taiwan with Counties - Multicolor</a>
     <span> by </span>
