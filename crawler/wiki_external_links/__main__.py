3e44a2e31a47e073b22f4a14d40941cf5dcee994
BangYuWen <bangyuwen@gmail.com>
2019-06-22 00:03:51 +0800

    filter by section

--- crawler/wiki_external_links/__main__.py
+++ crawler/wiki_external_links/__main__.py
@@ -1,10 +1,10 @@
 import json
-import re
 from multiprocessing.dummy import Pool
 from os import remove
 
 import requests
 
+from transform import transform
 from util import store_json
 
 URL = 'https://zh.wikipedia.org/w/api.php'
@@ -23,14 +23,15 @@ def _send_request(payload):
     print(f'[INFO] Sending request to {URL}, payload: {payload}')
     try:
         response = requests.get(URL, params=payload, timeout=10)
-    except requests.exceptions.ConnectTimeout:
-        exit('[ERROR] Request Timeout')
+        assert response.status_code == 200, f'[ERROR] Request Error, response code: {response.status_code}'
+        return response.text
+    except (requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout):
+        print(f'[ERROR] Request Timeout, payload: {payload}')
+    except AssertionError:
+        print(f'[ERROR] Request Error, status_code: {response.status_code}, payload: {payload}')
 
-    assert response.status_code == 200, f'[ERROR] Request Error, response code: {response.status_code}'
-    return response.text
 
-
-def get_infobox_page_list():
+def get_page_list():
     payload = {
         'action': 'parse',
         'format': 'json',
@@ -43,13 +44,36 @@ def get_infobox_page_list():
     return [link['*'] for link in response_data['parse']['links'] if not link['*'].startswith('Template')]
 
 
+def get_external_link_section(page_name):
+    payload = {
+        'action': 'parse',
+        'prop': 'sections',
+        'format': 'json',
+        'page': page_name
+    }
+    response_data = json.loads(_send_request(payload))
+    try:
+        sections = response_data['parse']['sections']
+        for section in sections:
+            if section['line'] == '外部連結':
+                return section['index']
+        else:
+            print(f'[WARRING] No sections, page_name: {page_name}')
+    except IndexError:
+        print(f'[WARRING] Can\'t find the last section, data: {response_data}')
+    except KeyError:
+        print(f'[WARRING] Page doesn\'t exist, page_name: {page_name}')
+
+
 def get_page_links(page_name):
+    section = get_external_link_section(page_name)
     payload = {
         'action': 'parse',
         'format': 'json',
         'page': page_name,
         'prop': 'externallinks',
-        'utf8': ''
+        'utf8': '',
+        'section': section
     }
     response_data = json.loads(_send_request(payload))
     try:
@@ -59,18 +83,8 @@ def get_page_links(page_name):
         return {'title': page_name}
 
 
-def transform(pages_links):
-    def classify_links(page):
-        return {
-            'title': page['title'],
-            'fb_links': [link for link in page.get('externallinks', []) if re.match(r'^https://www.facebook.com/[^/]+/?$', link)]
-        }
-
-    return [classify_links(page) for page in pages_links]
-
-
 if __name__ == "__main__":
-    page_names = get_infobox_page_list()
+    page_names = get_page_list()
     with Pool(processes=4) as pool:
         pages_links = pool.map(get_page_links, page_names)
     pages_links_string = json.dumps(pages_links, ensure_ascii=False)
