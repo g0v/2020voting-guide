eb6d6c61d28945e50ea501aa3611d9a054ceb653
喵喵 <bluesdollcat@gmail.com>
2019-06-29 18:12:31 +0800

    remove slide bar , add drawer

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import { Route, NavLink } from 'react-router-dom';
-import './App.css';
-import SlideBar from './components/SlideBar';
-import { Row, Col, Menu, Icon, Select } from 'antd';
-import './static/style/slideBar.scss';
+import './App.scss';
+import { Row, Col, Menu, Icon, Button, Drawer } from 'antd';
 import './static/style/button.scss';
 import MediaQuery from 'react-responsive';
 
@@ -15,29 +13,42 @@ interface AppState {
   whichPageToFresh: string;
 }
 
-class App extends React.Component<{}, AppState> {
+class App extends React.Component<{}> {
   constructor(props: {}) {
     super(props);
-    this.state = {
-      interval: 10, // sendons
-      whichPageToFresh: ''
-    };
   }
+  state = { visible: false };
+
+  showDrawer = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
 
   render() {
     return (
       <Row className="main-page" style={{ minHeight: window.innerHeight }}>
         <Row className="header">
-          <Col span={1} ></Col>
+          <Col span={1} >
+            <Button type="ghost" onClick={this.showDrawer}><Icon type="menu" style={{color: 'white'}}/></Button>
+          </Col>
           <Col span={22}>
             <Row>
                   <MediaQuery minDeviceWidth={300}>
                       <Row className="nav">
-                          <ul>
-                          <NavLink exact to="/home"><li className="appName">
-                                2020 投票指南
-                              </li></NavLink>
-                          </ul>
+                            <ul>
+                              <NavLink exact to="/home">
+                                <li className="appName">
+                                    2020 投票指南
+                                </li>
+                              </NavLink>
+                            </ul>
                       </Row>
                   </MediaQuery>
               </Row>
@@ -49,6 +60,17 @@ class App extends React.Component<{}, AppState> {
             <Route exact path="/home" component={Home} />
           </Row>
         </Row>
+        <Drawer
+          title="選區找立委"
+          placement="left"
+          closable={false}
+          onClose={this.onClose}
+          visible={this.state.visible}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Drawer>
       </Row>
     );
   }
