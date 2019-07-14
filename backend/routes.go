c91fb62c4c1eb61f9fa0ac1726f805178bf2aa5e
bangyuwen <bangyuwen@gmail.com>
2019-07-15 07:51:19 +0800

    revert import docs path

--- backend/routes.go
+++ backend/routes.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "./docs"
+	_ "github.com/bangyuwen/2020voting-guide/backend/docs"
 	"github.com/gin-gonic/gin"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
