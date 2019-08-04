c6ab797b2b92ec44007e0ec078575e0b5abbad86
bangyuwen <bangyuwen@gmail.com>
2019-08-05 00:07:14 +0800

    delete unuse

--- crawler/utils/util.py
+++ crawler/utils/util.py
@@ -1,5 +1,4 @@
 import os
-import pymysql.cursors
 
 # DB config
 MYSQL_HOST = os.environ["MYSQL_HOST"]
@@ -13,16 +12,3 @@ def store_json(json_data: str, output_path: str) -> None:
     with open(output_path, "w+") as fp:
         fp.write(json_data)
         print(f'[INFO] JSON data is written to "{output_path}"')
-
-
-def getDbConnection():
-    connection = pymysql.connect(
-        host=MYSQL_HOST,
-        user=MYSQL_USER,
-        password=MYSQL_PASSWORD,
-        db=MYSQL_DB,
-        charset=MYSQL_CHARSET,
-        cursorclass=pymysql.cursors.DictCursor,
-    )
-
-    return connection
