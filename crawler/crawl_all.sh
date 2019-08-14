1e6ff33da4f217d343dd734a7e5f155ffda28093
bangyuwen <bangyuwen@gmail.com>
2019-08-14 22:12:38 +0800

    rename and crawl all

--- crawler/crawl_all.sh
+++ crawler/crawl_all.sh
@@ -5,19 +5,10 @@ set -o errexit
 SOURCE_DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "${SOURCE_DIR}"
 
-# clean up folders because of renaming files
-find ../data/raw -type f \
-    -not -name 'README.md' \
-    -not -path '*/voteData/*' \
-    -delete
-find ../data/organized -type f \
-    -not -name 'README.md' \
-    -delete
-
 # crawl & transform to a useful format
 pipenv sync
-pipenv run python3 legislator_candidate
-pipenv run python3 legislator_info
-pipenv run python3 sitting_info
-pipenv run python3 wiki_infoboxes
+pipenv run python3 candidate
+pipenv run python3 candidate_age
+pipenv run python3 legislator
+pipenv run python3 sitting
 pipenv run python3 bulletin
