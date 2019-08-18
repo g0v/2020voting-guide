3dea9e440f737cbe7c422b26b62df3fc9c700c0a
bangyuwen <bangyuwen@gmail.com>
2019-08-18 23:49:34 +0800

    clean up data

--- preprocessing/sql/last_term.sql
+++
@@ -1,8 +0,0 @@
--- count last_term for candidate list
-
-select count(*), last_term
-from (
-    SELECT max(CAST(term as UNSIGNED)) as last_term, name FROM `legislator_record` where name in (select name from candidate) group by name order by last_term desc
-) as ttt
-group by last_term
-
