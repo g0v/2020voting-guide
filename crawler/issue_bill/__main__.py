36152d0085e7ae4a01940959757349fc9e1ee550
bangyuwen <bangyuwen@gmail.com>
2019-09-30 19:54:15 +0800

    add more category info

--- crawler/issue_bill/__main__.py
+++ crawler/issue_bill/__main__.py
@@ -1,23 +1,30 @@
 import json
 from os import path
-
+from glob import glob
 import pandas as pd
 
 from db import Bill
 
 FILE_DIR = path.dirname(path.abspath(__file__))
-EXCEL = f"{FILE_DIR}/issueBill.xlsx"
 
 if __name__ == "__main__":
-    for category in pd.ExcelFile(EXCEL).sheet_names:
-        df = pd.read_excel(EXCEL, sheet_name=category)
+    for excel in glob(f"{FILE_DIR}/../../data/manual/issueBill/*"):
+        category = path.basename(excel).replace(".xls", "")
+
+        df = pd.read_html(excel, header=[0])[0]  # source stores html table as .xls
         bills = df.to_json(orient="records", force_ascii=False)
+
         formated_bills = [
             {"term": bill["會期"][:2], "sessionPeriod": bill["會期"][3:5], "name": bill["提案名稱"], "category": category}
             for bill in json.loads(bills)
         ]
         print(formated_bills)
         for bill in formated_bills:
-            Bill.update(category=bill["category"]).where(
-                Bill.term == bill["term"], Bill.sessionPeriod == bill["sessionPeriod"], Bill.name == bill["name"]
-            ).execute()
+            affected_rows = (
+                Bill.update(category=bill["category"])
+                .where(
+                    Bill.term == bill["term"], Bill.sessionPeriod == bill["sessionPeriod"], Bill.name == bill["name"]
+                )
+                .execute()
+            )
+            print(f"Affected Rows: {affected_rows}")
