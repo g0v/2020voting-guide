e89a571f09019405e315dabaedbce4b19a3b8e77
bangyuwen <bangyuwen@gmail.com>
2019-10-18 09:43:55 +0800

    Extendable bill's caseOfAction
    As V2 design hidden overflow content over 4 lines

--- mobile/src/components/IssueBill.tsx
+++ mobile/src/components/IssueBill.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from '@material-ui/core';
-import { useTheme } from '@material-ui/core/styles';
+import { useTheme, makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import BillDialog from './BillDialog';
 export interface Bill {
@@ -22,6 +22,47 @@ interface IssueBillProps {
     bills: Bill[];
 }
 
+const useStyles = makeStyles({
+    billInfo: (open: boolean) => ({
+        height: open ? 'auto' : '5em',
+        overflow: 'hidden'
+    })
+});
+
+const Bill = ({
+    name,
+    description,
+    caseOfAction
+}: {
+    name: string;
+    description: string;
+    caseOfAction: string;
+}) => {
+    const [open, setOpen] = React.useState(false);
+    const classes = useStyles(open);
+    return (
+        <Box mt={3} key={name}>
+            <div
+                onClick={() => {
+                    setOpen(!open);
+                }}
+            >
+                <Typography variant="h3">{name}</Typography>
+                <Box my={1}>
+                    <Typography variant="body2" color="textSecondary">
+                        {description}
+                    </Typography>
+                </Box>
+                <Box my={1} className={classes.billInfo}>
+                    <Typography variant="body2" color="textSecondary">
+                        {caseOfAction}
+                    </Typography>
+                </Box>
+            </div>
+        </Box>
+    );
+};
+
 const IssueBill = ({ issue, bills }: IssueBillProps) => {
     const theme = useTheme();
 
@@ -30,19 +71,7 @@ const IssueBill = ({ issue, bills }: IssueBillProps) => {
             <Box mx={1.5} py={3}>
                 <Typography variant="h2">{issue}</Typography>
                 {bills.map(bill => (
-                    <Box mt={3} key={bill.name}>
-                        <Typography variant="h3">{bill.name}</Typography>
-                        <Box my={1}>
-                            <Typography variant="body2" color="textSecondary">
-                                {bill.description}
-                            </Typography>
-                        </Box>
-                        <Box my={1} height="5em" overflow="hidden">
-                            <Typography variant="body2" color="textSecondary">
-                                {bill.caseOfAction}
-                            </Typography>
-                        </Box>
-                    </Box>
+                    <Bill {...bill} />
                 ))}
             </Box>
             <Box p={1} bgcolor={theme.palette.background.default} />
