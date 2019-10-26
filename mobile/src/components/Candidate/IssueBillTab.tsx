dd2608d665845d2adbe2afedcf8ea728fe1133df
bangyuwen <bangyuwen@gmail.com>
2019-10-26 09:42:58 +0800

    Add floating filter button

--- mobile/src/components/Candidate/IssueBillTab.tsx
+++ mobile/src/components/Candidate/IssueBillTab.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Fab } from '@material-ui/core';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import FilterListIcon from '@material-ui/icons/FilterList';
 import Bulletin from '../Bulletin';
 import IssueBill, { Bill } from '../IssueBill';
 
@@ -6,8 +9,24 @@ interface IssueBillTab {
     bills?: Bill[];
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        fab: {
+            position: 'fixed',
+            bottom: theme.spacing(2),
+            right: theme.spacing(2)
+        },
+        extendedIcon: {
+            marginRight: theme.spacing(1)
+        }
+    })
+);
+
 const IssueBillTab = ({ bills = [] }: IssueBillTab) => {
-    const issues = [...new Set(bills.filter(b => b.category).map(b => b.category))];
+    const classes = useStyles();
+    const issues = [
+        ...new Set(bills.filter(b => b.category).map(b => b.category))
+    ];
     return (
         <>
             <Bulletin
@@ -21,9 +40,25 @@ const IssueBillTab = ({ bills = [] }: IssueBillTab) => {
                         bills: bills.filter(i => issue === i.category)
                     }))
                     .map(issue => (
-                        <IssueBill issue={issue.issue} bills={issue.bills} key={issue.issue}/>
+                        <IssueBill
+                            issue={issue.issue}
+                            bills={issue.bills}
+                            key={issue.issue}
+                        />
                     ))}
             </div>
+            <Fab
+                variant="extended"
+                size="small"
+                color="primary"
+                className={classes.fab}
+            >
+                <FilterListIcon
+                    fontSize="small"
+                    className={classes.extendedIcon}
+                />
+                篩選議題
+            </Fab>
         </>
     );
 };
