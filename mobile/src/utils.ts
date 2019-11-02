98aeeacde1477a7f12c1511b9a8590520bc058ba
milkmidi <milkmidi@gmail.com>
2019-11-02 13:46:02 +0800

    Complete Page Home

--- mobile/src/utils.ts
+++ mobile/src/utils.ts
@@ -1,3 +1,27 @@
+// cdn js
+declare let TweenMax: any;
+declare let Power2: any;
+
 export const simplifyCaseOfAction = (caseOfAction: string) => {
     return caseOfAction.replace(/^.+?，/, '').replace(/[，。]?爰(.+)$/, '。');
+};
+
+export const scrollBody = (dom: string, duration = 0.5, gap = 10) => {
+    const { clientHeight: headerHeight } = document.querySelector(
+        'header'
+    ) as HTMLDivElement;
+
+    const targetDOM: HTMLDivElement = document.querySelector(
+        dom
+    ) as HTMLDivElement;
+    const { top: targetRectTop } = targetDOM.getBoundingClientRect();
+    const scrollToY: number =
+        window.scrollY + targetRectTop - gap - headerHeight;
+    return TweenMax.to(window, duration, {
+        scrollTo: {
+            y: scrollToY,
+            autoKill: false
+        },
+        ease: Power2.easeOut
+    });
 };
