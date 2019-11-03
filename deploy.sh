1f64f1143355d1a129456810ebf5c1d234cad6b9
bangyuwen <bangyuwen@gmail.com>
2019-11-03 13:20:15 +0800

    Deploy to stage

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 set -o errexit
+set -o xtrace
+
+env=${1:-stage}
+
+set -o errexit
 
 cd ./mobile
 npm run build
 
 cd ../
-gcloud app deploy
+if [ "${env}" == "stage" ]; then
+    gcloud app deploy --no-promote
+else
+    gcloud app deploy
+fi
