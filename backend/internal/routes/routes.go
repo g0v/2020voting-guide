73e7708f8746dec061fa9835cf97084cf30c4b3a
bangyuwen <bangyuwen@gmail.com>
2019-11-07 22:02:04 +0800

    Vernacular add third reading filter

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -25,7 +25,7 @@ func SetupRouter() *gin.Engine {
 	api.GET("/bill/:id", handler.GetBillHandler)
 	api.GET("/statistic/:name", handler.GetStatisticByNameHandler)
 
-	api.GET("/vernacularlist/:page", handler.ListVernacular)
+	api.GET("/vernacularlist/:filter", handler.ListVernacular)
 	api.GET("/vernacular/:id", handler.GetVernacular)
 	api.POST("/vernacular/:id", handler.PostVernacular)
 
