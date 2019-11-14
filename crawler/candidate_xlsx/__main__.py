aa3934f8f885d523c90bdfce4ab783bf51e80c16
bangyuwen <bangyuwen@gmail.com>
2019-11-14 08:29:24 +0800

    Digest manual candidate

---
+++ crawler/candidate_xlsx/__main__.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+
+from db import ManualCandidate
+
+df = pd.read_csv("../data/manual/2020candidate.csv")
+df.columns = [
+    "name",  # 姓名
+    "constituency",  # 選區
+    "party",  # 政黨
+    "currentLegislator",  # 現任立委
+    "beenLegislator",  # 曾任立委
+    "dayOfBirth",  # 生日
+    "age",  # 年齡
+    "photo",  # 照片
+    "wiki",  # wiki
+    "fbPage",  # FB 粉絲專頁
+    "education",  # 學歷
+    "educationConn",  # 學歷連結
+    "experience",  # 經歷
+    "experienceConn",  # 經歷連結
+    "politics",  # 政見
+    "politicsConn",  # 政見連結
+]
+data = df.replace({np.nan: None}).to_dict(orient="records")
+ManualCandidate.drop_table()
+ManualCandidate.create_table()
+ManualCandidate.insert_many(data).execute()
