85dbd6e84cf47a6909a6483b1f01a35ce9d6d93e
debbyji <jasmine820921@gmail.com>
2019-11-16 00:24:10 +0800

    調整 barchart custom tooltip

--- mobile/src/components/Candidate/CompareBarChart.tsx
+++ mobile/src/components/Candidate/CompareBarChart.tsx
@@ -1,4 +1,3 @@
-// import { ResponsiveContainer } from '@material-ui/core';
 import React from 'react';
 import { Bar, BarChart, XAxis, YAxis, Tooltip, Legend, CartesianGrid, ResponsiveContainer } from "recharts";
 
@@ -25,9 +24,34 @@ const renderColorfulLegendText = (value: string, entry: any) => {
 	</span>;
 }
 
-const CompareBarChart = ({ data, name }: CompareBarChart) => {
+interface BasePaper {
+	active?: boolean;
+	label?: string;
+	payload?: { value: string; totalIncome: number; totalExpense: number }[];
+}
 
-	const currentName = name
+const CustomTooltip = ({active, label, payload}: BasePaper) => {
+  if (active) {
+
+		// const income = payload ? (Number(payload[0].value) * 10000000).toLocaleString() : 0;
+		// const expense = payload ? (Number(payload[1].value) * 10000000).toLocaleString() : 0;
+
+		const income = payload ? (Number(payload[0].value).toFixed(2) + '百萬') : 0;
+		const expense = payload ? (Number(payload[1].value).toFixed(2) + '百萬') : 0;
+    return (
+      <div className="custom-tooltip">
+				<p>{label}</p>
+        <p className="label">{`收入 : ${income}`}</p>
+        <p className="label">{`支出 : ${expense}`}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+
+const CompareBarChart = ({ data, name }: CompareBarChart) => {
 
 	const normalizeData = data.map(val => {
 		return {
@@ -42,13 +66,13 @@ const CompareBarChart = ({ data, name }: CompareBarChart) => {
 				<BarChart
 					data={normalizeData}
 					margin={{
-						top: 40, right: 80, bottom: 20, left: 20,
+						top: 40, right: 30, bottom: 20, left: 0,
 					}}>
 
 					<CartesianGrid strokeDasharray="3 3" />
 					<XAxis dataKey="name" />
 					<YAxis label={{ value: '百萬', angle: 0, position: 'top', offset: 20, fontSize: 14, fill: 'rgba(0, 0, 0, 0.54)' }}/>
-					<Tooltip />
+					<Tooltip content={<CustomTooltip/>}/>
         	<Legend formatter={renderColorfulLegendText} />
 					<Bar dataKey="totalIncome" fill="#3199BA" barSize={17} />
 					<Bar dataKey="totalExpense" fill="#D4AF37" barSize={17} />
