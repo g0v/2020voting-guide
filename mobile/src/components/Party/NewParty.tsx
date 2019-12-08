b45b74546945e0c569a138989ea5940230ea86d4
gnituy18 <sourfeethsu@gmail.com>
2019-12-09 00:18:05 +0800

    fix new party

--- mobile/src/components/Party/NewParty.tsx
+++ mobile/src/components/Party/NewParty.tsx
@@ -1,7 +1,7 @@
-import { Box } from '@material-ui/core';
+import { Typography, Box } from '@material-ui/core';
 import React from 'react';
 
-const NewParty = () => {
+const NewParty = ({ name }: { name: string }) => {
     return (
         <Box
             // bgcolor={theme.palette.background.default}
@@ -9,7 +9,14 @@ const NewParty = () => {
             textAlign="center"
             py={5}
         >
-            <img width="375" src="/img/new.svg" alt="施工中" />
+            <img width="275" src="/img/new.svg" />
+            <Typography variant="h2">{`${name}上屆沒有立委唷！`}</Typography>
+            <Typography variant="h4" color="textSecondary">
+                這個政黨沒有法案紀錄
+            </Typography>
+            <Typography variant="h4" color="textSecondary">
+                透過其他方法認識他們
+            </Typography>
         </Box>
     );
 };
