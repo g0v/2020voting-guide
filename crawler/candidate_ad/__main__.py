363ad64d80aec0596b05558b5eb16e0416fbffa3
bangyuwen <bangyuwen@gmail.com>
2019-12-16 23:52:15 +0800

    remove none content ad

--- crawler/candidate_ad/__main__.py
+++ crawler/candidate_ad/__main__.py
@@ -9,8 +9,8 @@ STATIC_DATA_FOLDER = "../data/static/ad"
 url = "https://gist.githubusercontent.com/ronnywang/dd781d031501d69f70ddba6a2bf88d9b/raw/29930a8246e73e92ad99646bb1f3ff4b0401a8c2/fb-ad.jsonl"
 response = requests.get(url)
 
-ads = [json.loads(ad) for ad in response.text.split("\n") if "廣告詳情" in ad]
-print(ads)
+ads = [json.loads(ad) for ad in response.text.split("\n") if ad]
+ads = [ad for ad in ads if "廣告詳情" in ad and "ad_creative_body" in ad.get("廣告詳情")]
 
 # # file version
 # with open(RAW) as fp:
