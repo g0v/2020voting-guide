7a0272398a639881a326cf3501222f5dc8cb2349
bangyuwen <bangyuwen@gmail.com>
2019-12-17 22:09:51 +0800

    Correct new party icon

--- mobile/src/components/Party/NewParty.tsx
+++ mobile/src/components/Party/NewParty.tsx
@@ -3,13 +3,8 @@ import React from 'react';
 
 const NewParty = ({ name }: { name: string }) => {
     return (
-        <Box
-            // bgcolor={theme.palette.background.default}
-            height="100vh"
-            textAlign="center"
-            py={5}
-        >
-            <img width="200" src="/img/new.svg" alt="new party" />
+        <Box bgcolor="#F7F7F7" height="100vh" textAlign="center" py={5}>
+            <img width="200" src="/img/doll/new_party.svg" alt="new party" />
             <Box mb={1}>
                 <Typography variant="h4">{`${name}上屆沒有立委唷！`}</Typography>
             </Box>
