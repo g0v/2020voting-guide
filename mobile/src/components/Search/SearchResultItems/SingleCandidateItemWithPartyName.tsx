cd62f0e2844e8481cef10653f9cbef89f19d5d7d
ms0223900 <ms0223900@gmail.com>
2019-12-29 17:39:11 +0800

    191229_Fix: fix candidate type related type issue

--- mobile/src/components/Search/SearchResultItems/SingleCandidateItemWithPartyName.tsx
+++ mobile/src/components/Search/SearchResultItems/SingleCandidateItemWithPartyName.tsx
@@ -1,42 +1,46 @@
-import React from 'react'
-import SingleCandidateItem from '../../Party/SingleCandidateItem'
-import { Candidate } from '../../Party/types'
-import { ListItem, Card, Typography, Divider, Box, makeStyles, Theme } from '@material-ui/core'
-import ResultItemCardWrapper from './ResultItemCardWrapper'
+import React from 'react';
+import SingleCandidateItem from '../../Party/SingleCandidateItem';
+import { Candidate } from '../../Party/types';
+import {
+    ListItem,
+    Card,
+    Typography,
+    Divider,
+    Box,
+    makeStyles,
+    Theme
+} from '@material-ui/core';
+import ResultItemCardWrapper from './ResultItemCardWrapper';
 
 const useStyles = makeStyles((theme: Theme) => ({
-  outer: {
-    paddingBottom: theme.spacing(2),
-  },
-  partyName: {
-    paddingBottom: theme.spacing(1),
-  }
-}))
+    outer: {
+        paddingBottom: theme.spacing(2)
+    },
+    partyName: {
+        paddingBottom: theme.spacing(1)
+    }
+}));
 
 type Props = {
-  candidate: Candidate
-}
-const SingleCandidateItemWithPartyName = ({
-  candidate
-}: Props) => {
-  const classes = useStyles()
-  return (
-    <ResultItemCardWrapper>
-      <ListItem
-        button
-        component={'a'}
-        href={`/party/${candidate.party}`}
-      >
-        <Box>
-          <Box className={classes.partyName}>
-            <Typography color={'textSecondary'}>{candidate.party}</Typography>
-          </Box>
-          <Divider />
-          <SingleCandidateItem candidate={candidate} />
-        </Box>
-      </ListItem>
-    </ResultItemCardWrapper>
-  )
-}
+    candidate: Candidate;
+};
+const SingleCandidateItemWithPartyName = ({ candidate }: Props) => {
+    const classes = useStyles();
+    return (
+        <ResultItemCardWrapper>
+            <ListItem button component={'a'} href={`/party/${candidate.party}`}>
+                <Box>
+                    <Box className={classes.partyName}>
+                        <Typography color={'textSecondary'}>
+                            {candidate.party}
+                        </Typography>
+                    </Box>
+                    <Divider />
+                    <SingleCandidateItem candidate={candidate} />
+                </Box>
+            </ListItem>
+        </ResultItemCardWrapper>
+    );
+};
 
-export default SingleCandidateItemWithPartyName
+export default SingleCandidateItemWithPartyName;
