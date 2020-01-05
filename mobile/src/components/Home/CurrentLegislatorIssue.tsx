96bd92b7b00ab829e2567a76bee845b4258a7821
Zach Chang <podeng0321@gmail.com>
2020-01-05 23:41:08 +0800

    add describtion in Home

---
+++ mobile/src/components/Home/CurrentLegislatorIssue.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Button, Typography, Box } from '@material-ui/core';
+
+const CurrentLegislatorIssue = () => {
+    return (
+        <Box my={4}>
+            <Typography variant="h2">猜猜誰最常看 交通建設 法？</Typography>
+            <Typography>選你在乎的議題  爭取連任的立委比一比</Typography>
+        </Box>
+    );
+};
+
+export default CurrentLegislatorIssue;
