b3976cae776369934702033628bd3f2e9ebf529d
milkmidi <milkmidi@gmail.com>
2020-01-06 18:22:25 +0800

    Update CandidateCompare education experience

--- mobile/src/components/CandidateCompare/CandidateCompareCardEducation.tsx
+++ mobile/src/components/CandidateCompare/CandidateCompareCardEducation.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
-import clsx from 'clsx';
-import useFetch from '../../hooks/useFetch';
-import { CandidateType } from '../Candidate/Candidate';
-
+import regionalAll from '../../data/cec_regional_all.json';
+import find from 'lodash/find';
 interface Props {
     name: string;
     constituency: string;
@@ -12,46 +9,39 @@ export default function CandidateCompareCardEducation({
     name,
     constituency
 }: Props) {
-    const { isLoading, responseData } = useFetch<CandidateType>(
-        `/api/candidate/${constituency}/${name}`,
-        {},
-        [name]
-    );
+    const [education, experience] = React.useMemo(() => {
+        const candidateData = find(regionalAll, { name }) as any;
+        return [
+            {
+                __html: candidateData.education.replace(/\n/g, '<br/>')
+            },
+            {
+                __html: candidateData.experience.replace(/\n/g, '<br/>')
+            }
+        ];
+    }, []);
 
-    let child = null;
-    if (!isLoading) {
-        child = (
-            <>
-                <div className="h4 mb-2">學歷</div>
-                <Typography variant="h5" color="textSecondary">
-                    {responseData.education || '即將更新'}
-                </Typography>
-                <div className="mb-3"></div>
-                <div className="h4 mb-2">經歷</div>
-                <Typography variant="h5" color="textSecondary">
-                    {responseData.experience || '即將更新'}
-                </Typography>
-                {(responseData.education || responseData.experience) && (
-                    <div className="mt-auto">
-                        <a
-                            className="btn btn-rounded"
-                            href={`/candidate/${constituency}/${name}?tab=3`}
-                        >
-                            詳細學經歷
-                        </a>
-                    </div>
-                )}
-            </>
-        );
-    }
     return (
-        <div
-            className={clsx(
-                'candidate-compare-col candidate-compare-education loading style-flex',
-                { 'is-show': isLoading }
-            )}
-        >
-            {child}
+        <div className="candidate-compare-col candidate-compare-education loading style-flex">
+            <div className="h4 mb-2">學歷</div>
+            <div
+                className="h5 text-overflow-5 color-gray"
+                dangerouslySetInnerHTML={education}
+            ></div>
+            <div className="mb-3"></div>
+            <div className="h4 mb-2">經歷</div>
+            <div
+                className="h5 text-overflow-5 color-gray"
+                dangerouslySetInnerHTML={experience}
+            ></div>
+            <div className="mt-auto">
+                <a
+                    className="btn btn-rounded"
+                    href={`/candidate/${constituency}/${name}?tab=3`}
+                >
+                    詳細學經歷
+                </a>
+            </div>
         </div>
     );
 }
