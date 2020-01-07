c0684df4c8e3ee649993d9033fc58ff17a27cc6a
bangyuwen <bangyuwen@gmail.com>
2020-01-07 22:19:35 +0800

    Revert "Compare Parties Style"
    This reverts commit 3c37a96b63ca883f3fb8c1868e0e3527323f9722.

--- mobile/src/components/PartiesCompare/PartiesCompareCardHeader.tsx
+++ mobile/src/components/PartiesCompare/PartiesCompareCardHeader.tsx
@@ -1,12 +1,12 @@
-import { Avatar, Box, IconButton } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
+import React, { useCallback } from 'react';
+import { makeStyles } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
-import { makeStyles } from '@material-ui/styles';
-import clsx from 'clsx';
-import find from 'lodash/find';
-import React, { useCallback } from 'react';
 import partiesData from '../../data/party.json';
 import { PartyCard as PartyCardType } from '../PartyCandidates/PartyCard';
+import clsx from 'clsx';
+import find from 'lodash/find';
 
 interface Props {
     name: string;
@@ -51,11 +51,11 @@ const PartiesCompareCardHeader = ({
             </IconButton>
             <Avatar src={partyData.logo} className={classes.photo} />
             <div>
-                <Typography variant="h3" gutterBottom>
+                <Typography variant="h3" color="textSecondary">
                     {name}
                 </Typography>
             </div>
-            <Box pb={1}>
+            <div>
                 <div
                     className={clsx('candidate-compare-header-current', {
                         'style-gray': isElectedPersonZero
@@ -66,11 +66,11 @@ const PartiesCompareCardHeader = ({
                         ? '無現任立委'
                         : `現任 ${totalElectedPerson} 席`}
                 </span>
-            </Box>
+            </div>
             {!isElectedPersonZero && (
-                <Box pb={1} className="font-thumb color-gray">
+                <div className="font-thumb color-gray">
                     {electedPersonNum} 席不分區、{regionalLegislatorsNum} 席區域
-                </Box>
+                </div>
             )}
             <div className="font-thumb color-gray">上屆得票率 {voteRate}</div>
         </div>
